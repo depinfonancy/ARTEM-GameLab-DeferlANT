47afbee8b986dca370b766bec572e68181ebd90e
Pierre-sur-GitHub <Pierre-sur-GitHub@users.noreply.github.com>
2019-04-23 11:20:41 +0200

    Path finding
    Deuxieme importation et le path finding fonctionne correctement (pour l'instant la fourmi passe a travers les murs)

---
+++ Assets/deplacement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class deplacement : MonoBehaviour
+{
+
+    NavMeshAgent nav;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+             //   if (!EventSystem.current.IsPointerOverGameObject())
+                {
+                    nav.SetDestination(hit.point);
+                }
+            }
+        }
+            GameObject.Find("VisuFourmi").transform.position = gameObject.transform.position;
+    }
+}
