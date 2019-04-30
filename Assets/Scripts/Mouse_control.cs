72c417efaefa6ec20d8d853032cab691a43ece6b
Pierre-sur-GitHub <Pierre-sur-GitHub@users.noreply.github.com>
2019-04-30 12:08:40 +0200

    Debut depot pheromones
    Au clic de souris un cercle apparait, la taille n'est pas changeable pour l'instant

---
+++ Assets/Scripts/Mouse_control.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_control : MonoBehaviour
+{
+    public GameObject PheromoneOrange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnPheromone();
+        }
+    }
+
+    // faire apparaitre un cercle de pheromones a la pos cliquee de la souris
+    void SpawnPheromone()
+    {
+        Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pz.z = 0;
+        GameObject bullet = Instantiate(PheromoneOrange, pz, Quaternion.identity);
+    }
+
+}
