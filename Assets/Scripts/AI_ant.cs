f727c96735460e7f797435f8869263958b4bd4f9
maximelatge <maxime.latge@gmail.com>
2019-05-03 16:01:02 +0200

    Gestion des tiles
    Tri et parcours des tiles ok
    pb pour transformer les coordonnées des tiles de local à global pour le navMesh

--- Assets/Scripts/AI_ant.cs
+++ Assets/Scripts/AI_ant.cs
@@ -1,24 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Tilemaps;
+using UnityEngine.EventSystems;
+
 
 public class AI_ant : MonoBehaviour
 {
 
-    private string facingDirection = "right";
+    //private string facingDirection = "right";
+
+    NavMeshAgent nav;
+    //private Animator anim_fourmi;
+    private bool m_reaching_point = false;
+
     //private Animator animator_component;
-    
+
+    //protected readonly int has_a_direction = Animator.StringToHash("reaching_point");
+
+    //method returning a new random destination
+    private Vector3 pickRandomTile()
+    {
+        //récupération d'un nombre aléatoire correspondant à un index de la liste des tiles accessibles
+        int randomNumber = (int)Random.Range(0, Tiles_Monitor.accesibleTilePositionList.Count - 1);
+        //récupéartion de la coordonnée globale de la tile choisie aléatoirement
+        Vector3 globalTilePosition = Tiles_Monitor.accesibleTilePositionList[0];
+        return globalTilePosition;
+    }
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        nav = GetComponent<NavMeshAgent>();
+        //anim_fourmi = gameObject.transform.parent.Find("VisuFourmi").GetComponent<Animator>();
+
+        Vector3 pos = pickRandomTile();
+        Debug.Log("position de la 1ère tile:"+pos);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //   if (!EventSystem.current.IsPointerOverGameObject())
+                {
+                    Debug.Log("hit.point" + hit.point);
+                    //nav.SetDestination(hit.point);
+                }
+            }
+        }
+        //GameObject.Find("VisuFourmi").transform.position = gameObject.transform.position;
+        
+
+    
     }
 }
---
+++ Assets/Scripts/Tiles_Monitor.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Tiles_Monitor : MonoBehaviour
+{
+    public static Tilemap tilemap;
+    public TileBase TilePlein; //Tile pleine à mettre en paramètre pour l'exclure de la liste des tiles accessibles
+    public static List<Vector3> accesibleTilePositionList = new List<Vector3>(); //Liste positions des tiles accessibles aux fourmis
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tilemap tilemap = GetComponent<Tilemap>();
+        
+        BoundsInt bounds = tilemap.cellBounds; //récupère le rectangle encadrant toutes les tiles disposées
+        TileBase[] tileArray = tilemap.GetTilesBlock(bounds); //renvoi toutes les tiles comprises dans "bounds"
+
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                TileBase tile = tileArray[x + y * bounds.size.x];
+                if (tile != null && tile.name != TilePlein.name)
+                {
+                    //coordonnées locales de la tile
+                    Vector3Int localTilePosition = new Vector3Int(x, y, 0);
+                    //conversion en coordonnées globales pour le navmesh
+
+                    //Vector3 globalTilePosition = tilemap.GetCellCenterWorld(localTilePosition);
+
+                    Vector3 globalTilePosition = tilemap.CellToWorld(localTilePosition);
+                    Vector3 globalBoundPosition = tilemap.CellToWorld(bounds.position);
+
+                    globalTilePosition = (globalBoundPosition - globalTilePosition)/2;
+
+                    //Debug.Log("bounds.position.x :" + bounds.position.x + " bounds.position.y :" + bounds.position.y);
+                    //globalTilePosition.x = bounds.position.x - globalTilePosition.x;
+                    //globalTilePosition.y = bounds.position.y - globalTilePosition.y;
+
+                    accesibleTilePositionList.Add(globalTilePosition);
+                    //Debug.Log("AddedTile:" + tile.name);
+                }
+            }
+        }
+
+        //Debug.Log("accesibleTilePositionList.Count: " + accesibleTilePositionList.Count);
+        /*
+        foreach (Vector3 positionArray in accesibleTilePositionList)
+        {
+            Debug.Log("x : " + positionArray[0] + " y : " + positionArray[1]);
+        }
+        */
+
+
+
+        //Affiche la taille de bounds en nombre de tile
+        //Debug.Log("bounds.size.x : " + bounds.size.x + "\n bounds.size.y : " + bounds.size.y);
+
+        //Affiche dans la console toutes les tiles : leurs positions et leurs noms
+        //Le (0,0) est en bas à gauche
+        /*
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                TileBase tile = tileArray[x + y * bounds.size.x];
+                if (tile != null)
+                {
+                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                }
+                else
+                {
+                    Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                }
+            }
+        }
+        */
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
