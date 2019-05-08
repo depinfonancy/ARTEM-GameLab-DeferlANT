2e2d20bdb307e0a7c8aad5888e3d92f8408ea024
Pierre-sur-GitHub <Pierre-sur-GitHub@users.noreply.github.com>
2019-05-08 15:51:28 +0200

    scaling pheromone
    On choisit maintenant la taille de la pheromone quand on la cree. La taille du cercle ne suit pas exactement la souris mais le code est facile. A modifier si besoin.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,46 +4,58 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+    // deplacement de l'ecran avec la souris
     public float panSpeed = 20f;
     public float panBorderThickness = 10f;
-
+    // limites de la carte
     public Vector2 panLimit;
 
+    // reglages du zoom
     public float scrollSpeed = 20f;
     public float maxZoom = 5f;
     public float minZoom = 40f;
 
 
-    // Update is called once per frame
+
+
     void Update()
     {
         Vector3 pos = transform.position;
         float zoom = GetComponent<Camera>().orthographicSize;
 
+        // haut
         if (Input.GetKey("s") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             pos.y += panSpeed * Time.deltaTime;
         }
+        // bas
         if (Input.GetKey("w") || Input.mousePosition.y <= panBorderThickness)
         {
             pos.y -= panSpeed * Time.deltaTime;
         }
+        // gauche
         if (Input.GetKey("q") || Input.mousePosition.x <=panBorderThickness)
         {
             pos.x -= panSpeed * Time.deltaTime;
         }
+        // droite
         if (Input.GetKey("x") || Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
             pos.x += panSpeed * Time.deltaTime;
         }
 
+
+        // commande du zoom
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         zoom -= scroll * 100f * scrollSpeed * Time.deltaTime;
 
+        // comparaison avec les limites, si c'est plus loin rien ne se passe
         zoom = Mathf.Clamp(zoom, maxZoom, minZoom);
         pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.y);
         pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
 
+        // actualisation des composants
         GetComponent<Camera>().orthographicSize = zoom;
         transform.position = pos;
     }
--- Assets/Scripts/Mouse_control.cs
+++ Assets/Scripts/Mouse_control.cs
@@ -6,27 +6,52 @@ public class Mouse_control : MonoBehaviour
 {
     public GameObject PheromoneOrange;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    //public float minimumInitialScale;
+    private Vector3 posInit;
+    GameObject pheromone;
 
-    }
+    // marque quand le bouton de la souris est enfonce
+    bool enCours = false;
 
-    // Update is called once per frame
+
+
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        // a l'appui sur le bouton gauche de la souris un cercle de pheromone apparait
+        if (Input.GetMouseButtonDown(0))
         {
+            enCours = true;
             SpawnPheromone();
         }
+
+
+        // la pheromone en cours devient independante de la souris
+        if (Input.GetMouseButtonUp(0))
+        {
+            enCours = false;
+            pheromone = null;
+        }
+
+
+        // update de la taille de la pheromone avec la souris
+        if (enCours)
+        {
+            Vector3 posFin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float dist = Vector3.Distance(posFin, posInit);
+            pheromone.transform.localScale = dist * new Vector3(1, 1, 1);
+        }
+
     }
+
+
 
     // faire apparaitre un cercle de pheromones a la pos cliquee de la souris
     void SpawnPheromone()
     {
-        Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        pz.z = 0;
-        GameObject bullet = Instantiate(PheromoneOrange, pz, Quaternion.identity);
+        posInit = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        posInit.z = 0;
+        pheromone = Instantiate(PheromoneOrange, posInit, Quaternion.identity);
+        pheromone.SetActive(true);
     }
 
 }
