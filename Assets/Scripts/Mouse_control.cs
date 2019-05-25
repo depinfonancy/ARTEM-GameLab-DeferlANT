8422c2825bdbf4c8969db52058fe4c5177d8d7fe
Pierre-sur-GitHub <Pierre-sur-GitHub@users.noreply.github.com>
2019-05-25 15:23:18 +0200

    Revert "Merge branch 'master' into Creusage-fourmilière-Pierre"
    This reverts commit c9725d0d46e80895995507a740f93a33fd0dbb73, reversing
    changes made to 2f0fac36bfc2c778214758660831273195100699.

--- Assets/Scripts/Mouse_control.cs
+++ Assets/Scripts/Mouse_control.cs
@@ -32,9 +32,9 @@ public class Mouse_control : MonoBehaviour
     public TileBase intersection;
 
     
+
+    public GameObject PheromoneOrange;
     
-    
-    public GameObject Pheromone;
 
     //public float minimumInitialScale;
 
@@ -53,7 +53,6 @@ public class Mouse_control : MonoBehaviour
         //Tilemap tilemap = GetComponent<Tilemap>();
         posCursor = GetComponent<Transform>().position;
     }
-
 
 
     void Update()
@@ -153,13 +152,7 @@ public class Mouse_control : MonoBehaviour
         if (enCours)
         {
             float dist = Vector3.Distance(posFin, posInit);
-            pheromone.transform.Find("Pheromone_Range").transform.localScale = dist * new Vector3(0.1f, 0.1f, 0.1f);
-
-            //Ici il faut modifier le collider pour qu'il match avec le sprite, il faut trouver le bon rapport ou la bonne méthode
-            /*
-            CircleCollider2D collider = pheromone.transform.Find("Pheromone_Range").GetComponent<CircleCollider2D>();
-            collider.radius = dist*0.1f;
-            */
+            pheromone.transform.localScale = dist * new Vector3(1, 1, 1);
         }
 
     }
@@ -171,7 +164,7 @@ public class Mouse_control : MonoBehaviour
     {
         posInit = origin;
         posInit.z = 0;
-        pheromone = Instantiate(Pheromone, posInit, Quaternion.identity);
+        pheromone = Instantiate(PheromoneOrange, origin, Quaternion.identity);
         pheromone.SetActive(true);
     }
 
